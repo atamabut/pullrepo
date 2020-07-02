7dbe7ecf10fb86c99d0f9182b3bb1bc8ed92cb4a
Tamabut <atamabut@gmail.com>
2020-07-02 19:28:47 +0300

    modi-dev

--- Student.java
+++ Student.java
@@ -5,10 +5,14 @@ public class Student {
 	 int age = 20;
 	 String name = "John";
 	 String studentClass = "Form 1";
+	 String address = "Nairobi";
+	 String admissionNumber = "GHDF";
 	 
 	 System.out.println("Name:"+name);
 	 System.out.println("Age:"+age);
 	 System.out.println("Name:"+studentClass);
+	 System.out.println("Address:"+address);
+	 System.out.println("Admission:"+admissionNumber);
 	
 	 }
 	
