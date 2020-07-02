889c7c6311b2aae75d2f064927e6fc5338bdcc24
Tamabut <atamabut@gmail.com>
2020-07-02 19:06:42 +0300

    my pull code

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+	
+	 public static void main(String[] args){
+	 
+	 int age = 20;
+	 String name = "John";
+	 String studentClass = "Form 1";
+	 
+	 System.out.println("Name:"+name);
+	 System.out.println("Age:"+age);
+	 System.out.println("Name:"+studentClass);
+	
+	 }
+	
+}
